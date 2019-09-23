e900d8a4aadc4c7b757d6cf26ea0432188365475
devjoaosantos <joao.santos.farias.2003@gmail.com>
2019-09-23 17:18:36 -0300

    23/09/2019

---
+++ faixa_etaria/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace faixa_etaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano = 0;
+            int idade = 0;
+
+            Console.WriteLine("Iremos dizer a sua faixa etária de acorno com o ano de seu nascimento!");
+            do{ 
+                Console.WriteLine("Digite seu ano de nascimento: "); 
+                ano = int.Parse(Console.ReadLine());
+                if(ano >2021){
+                Console.WriteLine("Data Inválida");
+                }
+            } while((ano > 2021) || (ano < 1850));
+
+            idade = 2019 - ano;
+
+            if(idade < 3 ) {
+                Console.WriteLine($" Você tem {idade} anos e você é um Recém-Nascido");
+            } else if ((idade >= 3) && (idade <= 11)){
+                Console.WriteLine($" Você tem cle{idade} anos e você é uma Criança");
+            } else if ((idade >= 12) && (idade <= 19)){
+                Console.WriteLine($" Você tem {idade} anos e você é um adolescente");
+            } else if ((idade >= 20) && (idade <= 65)){
+                Console.WriteLine($" Você tem {idade} anos e você é um adulto");
+            } else if (idade > 65) {
+                Console.WriteLine($"Você tem {idade} anos e você é um idoso");
+            }
+
+        }
+    }
+}
---
+++ login_usuario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace login_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login ;
+            string senha ;
+
+            Console.WriteLine("LOGIN: ");
+            login = Console.ReadLine();
+            Console.WriteLine("SENHA: ");
+            senha = Console.ReadLine();
+
+            if ((login == "admin") && (senha == "admin")){
+                Console.WriteLine("Bem Vindo Administrador!");
+            } else {
+                Console.WriteLine("Bem Vindo Usuário!");
+            }
+
+        }
+    }
+}
