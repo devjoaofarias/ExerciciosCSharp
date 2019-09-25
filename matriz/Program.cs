f15a752869bc573c5bd5821cbba8bb5be17e6382
devjoaosantos <joao.santos.farias.2003@gmail.com>
2019-09-25 17:24:02 -0300

    25/09/19

---
+++ matriz/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] vetor = new int[6];
+                Console.WriteLine("Digite 6 numeros para mim dizer se é impar ou par: ");
+                Console.WriteLine("Primeiro número: ");
+                vetor[0] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Segundo número: ");
+                vetor[1] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Terceiro número: ");
+                vetor[2] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Quarto número: ");
+                vetor[3] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Quinto número: ");
+                vetor[4] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Sexto número: ");
+                vetor[5] = int.Parse(Console.ReadLine());
+
+                float op1 = vetor[0] % 2;
+                float op2 = vetor[1] % 2;
+                float op3 = vetor[2] % 2;
+                float op4 = vetor[3] % 2;
+                float op5 = vetor[4] % 2;
+                float op6 = vetor[5] % 2;
+
+                if ( op1 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[0]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[0]}' é impar");
+               }
+
+                if ( op2 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[1]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[1]}' é impar");
+               }
+
+                if ( op3 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[2]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[2]}' é impar");
+               }
+
+                if ( op4 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[3]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[3]}' é impar");
+               }
+
+                if ( op5 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[4]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[4]}' é impar");
+                }
+
+                if ( op6 == 0) {
+                    Console.WriteLine($"O primeiro numero '{vetor[5]}' é par");
+               } else { 
+                   Console.WriteLine($"O primeiro numero '{vetor[5]}' é impar");
+               }
+              
+                
+                
+
+
+               
+            }
+
+        }
+    }
+
---
+++ testes/teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace teste
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+       
+
+        }
+    }
+}
