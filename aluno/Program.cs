2b289e1500c8b7570cdaa5dc271d0714da989931
devjoaosantos <joao.santos.farias.2003@gmail.com>
2019-10-07 17:18:51 -0300

    07/10/19

--- aluno/Program.cs
+++ aluno/Program.cs
@@ -1,42 +1,37 @@
 ﻿using System;
 
-namespace aluno
+namespace SaladeAula
 {
     class Program
     {
         static void Main(string[] args)
         {
-            aluno aluno1 = new aluno("João","456.333.222-11");
-            Console.WriteLine("Nome: " + aluno1.nome);
-            Console.WriteLine("Cpf: " + aluno1.cpf);
-
-            aluno aluno2 = new aluno("João","456.333.222-11","Desenvolvimento de sistemas");
-            Console.WriteLine("Nome: " + aluno2.nome);
-            Console.WriteLine("Cpf: " + aluno2.cpf);
-            Console.WriteLine("Curso: " + aluno2.curso);
-
-            Console.Write("Entre com o Nome: ");
-            nome = Console.ReadLine();
-            Console.Write("Entre com o CPF: ");
-            cpf = Console.ReadLine();
-            Console.Write("Entre com o curso: ");
-            curso = Console.ReadLine();
-            Console.Write("Entre com a nota: ");
-            int nota = int.Parse(Console.ReadLine());
-            Console.Write("Entre com o bimestre: ");
-            int bimestre = int.Parse(Console.ReadLine());
-            aluno aluno = new aluno(nome, cpf, curso);
-            aluno3.setnotas(bimestre, nota);    
-
-            Console.WriteLine("Nome: " + aluno3.nome);
-            Console.WriteLine("CPF: " + aluno3.cpf);
-            Console.WriteLine("Curso: " + aluno3.curso);
-            Console.WriteLine("Notas: ");
-            
-            foreach(int n in aluno3.getnotas()) {
-                Console.Write(n + " ");
+            Aluno aluno1 = new Aluno("Alexandre","123.456.789-0","Prog1");
+            Aluno aluno2 = new Aluno("Cesar", "987.654.321-9");
+            string nome_do_aluno = aluno1.nome;
+            string cpf_do_aluno = aluno1.cpf;
+            Console.WriteLine($"O nome do aluno1 é {nome_do_aluno}");
+            Console.WriteLine($"O CPF do aluno1 é: {cpf_do_aluno}");
+            Console.WriteLine($"O curso do aluno1 é: {aluno1.curso}");
+            double[] notas = aluno1.getNotas();
+            Console.Write("Notas: ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(notas[i] + " ");
             }
             Console.WriteLine();
+            aluno1.setNotas(6,8.0);
+
+            Console.Write("Notas: ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(notas[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"O nome do aluno2 é {aluno2.nome}");
+            Console.WriteLine($"O CPF do aluno2 é: {aluno2.cpf}");
+            Console.WriteLine($"O curso do aluno2 é: {aluno2.curso}");
         }
     }
 }
--- aluno/aluno.cs
+++ aluno/aluno.cs
@@ -1,39 +1,64 @@
-using system; 
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace aluno
+namespace SaladeAula
 {
-    public class aluno {
-        // Propriedades
-        public string nome{get;set;}
-        public string curso{get;set;}
-        public string cpf{get;set;}
-        public int[] notas;
+    class Aluno
+    {
+        //Atributos da Classe Alunos.
+        public string nome { get; set; }    //Nome do aluno
+        public string dtNasc { get; set; }  //Data de Nascimento do Aluno
+        public string cpf { get; set; }     //Documento de identificação do Aluno
+        public string curso { get; set; }   //Curso.
+        private double[] notas = { 0, 0, 0, 0 };    //Vetor contendo as notas bimestrais
 
-        // Construtores
-        public aluno(string nome, string cpf){
+        //Construtores
+        public Aluno(string nome,string cpf)
+        {
             this.nome = nome;
-            this.cpf = cpf;
+            this.cpf  = cpf;
         }
-        public aluno(string nome, string cpf, string curso){
+
+        public Aluno(string nome, string cpf, string curso)
+        {
             this.nome = nome;
             this.cpf = cpf;
             this.curso = curso;
+        }
 
-        // Metodos de acesso
-        public int[] getnotas(){return notas;}
-        public void setnotas (int bi, int nota){
-            int i = bi - 1;
-            if ((i < 0) || (i > 3)){
-                throw new ArgumentOutOfRangeException($"")
-                //Condiçao de erro;
-                //Condiçao de erro;
+        //Métodos de Acesso
+        public double[] getNotas()
+        {
+            return notas;
+        }
+
+        public void setNotas(int bimestre, double nota)
+        {
+            int i = bimestre - 1;
+            if ((i < 0) || (i > 4))
+            {
+                //Condição de Erro;
+                throw new ArgumentOutOfRangeException($"{nameof(setNotas)} must be between 1 and 4.");
             }
-
+            else if (nota < 0 || nota > 10)
+            {
+                //Condição de Erro;
+                throw new ArgumentOutOfRangeException($"{nameof(setNotas)} must be between 0 and 10.");
+            }
+            else
+            {
+                notas[i] = nota;
+            }
         }
+   
+        //Metodos
+        void Estudar() { }
+        void Conversar() { }
+        void Perguntar() { }
+        void Chorar_Prazos() { }
+        void Chorar_Notas() { }
+        void Chorar_Pelos_Cantos() { }
 
-        
-
-        }
     }
-        
 }
