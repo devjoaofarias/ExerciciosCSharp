98e11cae65903ad427087a4f80b8fb7ada85182e
devjoaosantos <joao.santos.farias.2003@gmail.com>
2019-11-08 17:23:24 -0300

    08/11/19

---
+++ MVC/McBonaldsMVC/Controllers/ClienteController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace McBonaldsMVC.Controllers {
+    public class ClienteController : Controller {
+        public IActionResult Login () {
+            return View ();
+        }
+    }
+}
